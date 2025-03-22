1ccf2d5c90c167ab3475e27f729b0fdca37c36fd
N.Z. Timisela <noatimisela@gmail.com>
2025-03-22 16:20:14 +0100

    Add: ChatPromptTemplate for the user to talk with the AI

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+# Chat start by user input
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+# Model used
+chain = LLMChain(
+    llm=chat, 
+    prompt=prompt
+)
+
+# The AI chat
 while True:
     content = input(">>")
-    print(f"You entered: {content} ")
+    result = chain({"content": content}) 
+    print(result["text"])
